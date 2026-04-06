b83f19c36e2be43590101aab7fad4587a2b63880
Haarish145 <haarishk2646@gamil.com>
2026-04-06 15:54:01 +0530

    Add tempe.java for temperature conversion and difference calculation

---
+++ tempe.java
@@ -0,0 +1,10 @@
+public class tempe {
+    public static void main(String[] args) {
+        double temp1 = 36.78;
+        int temp2 = (int) temp1;
+        double difference = temp1 - temp2;
+        System.out.println(temp1);
+        System.out.println(temp2);
+        System.out.println(difference);
+    }
+}
