335002f59b1777775951eda4e201b4109c84e09e
Haarish145 <haarishk2646@gamil.com>
2026-04-06 16:04:07 +0530

    Remove unnecessary output statements in fuel.java for cleaner console output

--- fuel.java
+++ fuel.java
@@ -5,9 +5,6 @@ public class fuel {
         double fuelPrice = 100.0; 
         double fuelUsed = distance / mileage;
         double totalCost = fuelUsed * fuelPrice;
-        System.out.println("Distance: " + distance + " km");
-        System.out.println("Mileage: " + mileage + " km/l");
-        System.out.println("Fuel Price: ₹" + fuelPrice);
         System.out.println("Fuel Used: " + fuelUsed + " liters");
         System.out.println("Total Fuel Cost: ₹" + totalCost);
     }
